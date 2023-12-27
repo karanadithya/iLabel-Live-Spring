76c7059a8cb50a73c44790ede5f39e18a3fe7c83
Karan <karanvb@outlook.com>
2023-12-27 11:28:50 +0530

    changed security configuration

--- src/main/java/holdings/indsys/iLabel/Security/config/SecurityConfiguration.java
+++ src/main/java/holdings/indsys/iLabel/Security/config/SecurityConfiguration.java
@@ -8,17 +8,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
-
-import static holdings.indsys.iLabel.Security.user.Permission.*;
-import static holdings.indsys.iLabel.Security.user.Role.ADMIN;
-import static holdings.indsys.iLabel.Security.user.Role.MANAGER;
-import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 @EnableWebSecurity
@@ -30,22 +24,13 @@ public class SecurityConfiguration {
     private final AuthenticationProvider authenticationProvider;
     private final LogoutHandler logoutHandler;
 
-    /**
-     * Configures the security filter chain for the HTTP requests.
-     *
-     * @param http the HttpSecurity object to configure
-     * @return the configured SecurityFilterChain
-     * @throws Exception if an error occurs during configuration
-     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                // Disable CSRF protection
-                .csrf(AbstractHttpConfigurer::disable)
-                // Allow access to certain endpoints without authentication
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                // API documentation endpoints
+                                "/api/v1/auth/**",
                                 "/v2/api-docs",
                                 "/v3/api-docs",
                                 "/v3/api-docs/**",
@@ -58,32 +43,20 @@ public class SecurityConfiguration {
                                 "/swagger-ui.html"
                         )
                         .permitAll()
-                        // Allow access to management endpoints for ADMIN and MANAGER roles
-                        .requestMatchers("/api/v1/management/**").hasAnyRole(ADMIN.name(), MANAGER.name())
-                        // Allow read access to management endpoints for ADMIN_READ and MANAGER_READ authorities
-                        .requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
-                        // Allow to create access to management endpoints for ADMIN_CREATE and MANAGER_CREATE authorities
-                        .requestMatchers(POST, "/api/v1/management/**").hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name())
-                        // Allow update access to management endpoints for ADMIN_UPDATE and MANAGER_UPDATE authorities
-                        .requestMatchers(PUT, "/api/v1/management/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name())
-                        // Allow to delete access to management endpoints for ADMIN_DELETE and MANAGER_DELETE authorities
-                        .requestMatchers(DELETE, "/api/v1/management/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name())
-                        // Require authentication for any other request
+//                        .requestMatchers("/api/v1/management/**").hasAnyRole(ADMIN.name(), MANAGER.name())
+//                        .requestMatchers("/api/v1/management/**").hasAnyRole(ADMIN.name(), MANAGER.name())
+//                        .requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
+//                        .requestMatchers(POST, "/api/v1/management/**").hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name())
+//                        .requestMatchers(PUT, "/api/v1/management/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name())
+//                        .requestMatchers(DELETE, "/api/v1/management/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name())
                         .anyRequest()
                         .authenticated())
-                // Set session management to STATELESS
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                // Set the custom authentication provider
                 .authenticationProvider(authenticationProvider)
-                // Add the JWT authentication filter before the UsernamePasswordAuthenticationFilter
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                // Configure logout endpoint and handlers
                 .logout(logout -> logout.logoutUrl("/api/v1/auth/logout")
                         .addLogoutHandler(logoutHandler)
-                        .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext()))
-                // Enable HTTP Basic authentication
-                .httpBasic(Customizer.withDefaults());
-        // Build and return the configured SecurityFilterChain
+                        .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext()));
         return http.build();
     }
 }
