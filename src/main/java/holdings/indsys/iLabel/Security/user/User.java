eec8f318f3b99c2f92ee2f293b9d4cc34fbb1cbd
Karan <karanvb@outlook.com>
2023-12-27 10:34:10 +0530

    API Authentication URL changed

--- src/main/java/holdings/indsys/iLabel/Security/user/User.java
+++ src/main/java/holdings/indsys/iLabel/Security/user/User.java
@@ -30,6 +30,7 @@ public class User implements UserDetails {
     private String email;
     private String password;
     private String phoneNumber;
+    private OTP otp;
 
     private Role role = Role.ADMIN;
 
